d40fd57157838fa9959f01cf5a812505757517b8
Rouslan Gaisin <rouslan.gaisin@gmail.com>
2019-02-14 16:19:16 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,47 @@
+# Instacheck: fair instagram giveaways random winner finder
+
+This script will help you to fairly find winner in giveaways. Giveaways - instagram lotterys with certain conditions. Usually participants have to  
+1. tag two of their friends in post's comment
+2. like the post
+3. subscribe to giveaway's author  
+Script is selecting all users, who have met the conditions and select one random user from them. Of course, it select's only unique users, so if someone will post multiple comments, he'll have same chances as if he posted one.
+
+### How to install
+
+Python 3 has to be already installed.  
+  
+After cloning the repository, use `pip` (or `pip3` in case of conflicts with Python 2) to install dependencies:
+```
+pip install -r requirements.txt
+```
+
+### How to use
+
+Firstly, you need an Insatgram account. Put username and password to a .env file, check ```.env_example``` as example. Better not use you personal account, since it can be banned. After finishing with an account, you can run a script.  
+  
+Script has two required parameters: 
+1. Link to the post, i.e. https://www.instagram.com/p/Bt28gc1hMCA/
+2. Author's username, i.e. lauravevere
+```
+$ python main.py https://www.instagram.com/p/Bt28gc1hMCA/ lauravevere
+python main.py https://www.insta
+gram.com/p/Bt28snYnmMB/ rebecca_rose_stylist
+2019-02-14 15:04:30,773 - INFO - Instabot Started
+2019-02-14 15:04:32,514 - INFO - Logged-in successfully as 'kibersnatch'!
+2019-02-14 15:05:51,860 - ERROR - Request returns 429 error!
+2019-02-14 15:05:51,861 - WARNING - That means 'too many requests'. I'll go to s
+leep for 5 minutes.
+2019-02-14 15:12:17,762 - ERROR - Request returns 429 error!
+2019-02-14 15:12:17,763 - WARNING - That means 'too many requests'. I'll go to s
+leep for 5 minutes.
+Getting followers of 49555342:  83%|███?| 19820/23772 [00:33<00:06, 627.67it/s]
+Waiting 2.02 min. due to too many requests.
+Getting followers of 49555342: 100%|███?| 23770/23772 [02:47<00:00, 141.53it/s]
+The winner is bridt89, profile link: https://instagram.com/bridt89
+2019-02-14 15:20:11,407 - INFO - Bot stopped. Worked: 20:33:53.495813
+2019-02-14 15:20:11,408 - INFO - Total requests: 2299
+```  
+As the result you get the winner's username: bridt89.  
+  
+**Warning**  
+Since the library simulates the user's input from a mobile device, the bot can take a pause for five minutes in case of a large number of requests. Don't worry about that.
---
+++ main.py
@@ -0,0 +1,81 @@
+from instabot import Bot
+import os
+import re
+import random
+from dotenv import load_dotenv
+import argparse
+
+
+def get_tagged_users(comments, bot):
+    mask = "@([a-zA-Z0-9._]{1,100})"
+    tagged_users = []
+    for comment in comments:
+        tagged_usernames = re.findall(mask, comment['text'])
+        real_usernames = [username for username in tagged_usernames if is_user_exists(username, bot)]
+        if len(real_usernames) >= 2:
+            tagged_users.append((str(comment['user_id']), comment['user']['username']))
+    return tagged_users
+
+
+def is_user_exists(username, bot):
+    user_id = bot.get_user_id_from_username(username)
+    if user_id is None:
+        return False
+    else:
+        return True
+
+
+def get_liked_users(media_id, users, bot):
+    liked_users = []
+    likers = bot.get_media_likers(media_id)
+    for user in users:
+        if user[0] in likers:
+            liked_users.append(user)
+    return liked_users
+
+
+def get_followed_users(username, users, bot):
+    followed_users = []
+    followers = bot.get_user_followers(username)
+    for user in users:
+        if user[0] in followers:
+            followed_users.append(user)
+    return followed_users
+
+
+def get_random_user(users):
+    random_number = random.randint(0, len(users)-1)
+    return users[random_number]
+
+
+def main():
+    parser = argparse.ArgumentParser(description='This is a Instagram giveaway '\
+                                                 'winner checker.')
+    parser.add_argument('post_link', help='Url of instagram post')
+    parser.add_argument('username', help="Post's author's instagram username")
+    args = parser.parse_args()
+    post_link = args.post_link
+    username = args.username
+
+    load_dotenv()
+    INSTA_USERNAME = os.getenv('INSTA_USERNAME')
+    INSTA_PASSWORD = os.getenv('INSTA_PASSWORD')
+    bot = Bot()
+    bot.login(username=INSTA_USERNAME, password=INSTA_PASSWORD)
+    media_id = bot.get_media_id_from_link(post_link)
+    comments = bot.get_media_comments_all(media_id)
+    tagged_users = get_tagged_users(comments, bot)
+    unique_tagged_users = set(tagged_users)
+    liked_users = get_liked_users(media_id, unique_tagged_users, bot)
+    followed_users = get_followed_users(username, liked_users, bot)
+    if followed_users:
+        winner = get_random_user(followed_users)
+        print('The winner is {}, profile link: https://instagram.com/{}'.format(
+                                                            winner[1], 
+                                                            winner[1]))
+    else:
+        print('No one met all the requirements')
+
+
+if __name__ == '__main__':
+    main()
